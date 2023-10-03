166d7b84acc50f4044542cf88c1d2089d9911fba
handeche <dechen.han@mail.utoronto.ca>
2023-10-03 14:36:04 -0400

    update unittest.cpp && add tests for scan()

--- src/unittest.cpp
+++ src/unittest.cpp
@@ -31,6 +31,70 @@ void inorderColor(Node* node, int indent=0)
     }
 }
 
+void test_get()
+{   
+    RBTree memtable(2); // Example with memtable_size 6
+    cout << "--- test case 1: Test get() from memtable ---" << endl;
+    // random insert
+    memtable.put(1, 10);
+    memtable.put(5, 50);
+    double key = 5;
+    double value = memtable.get(key);
+    if (value != -1) {
+        cout << "Found {Key: " << key << ", Value: " << value << "}" << endl;
+    } else {
+        cout << "Not found Key: " << key << endl;
+    }
+    cout << "\n--- test case 2: Test get() from SST ---" << endl;
+    memtable.put(2, 80);
+    value = memtable.get(key);
+    if (value != -1) {
+        cout << "Found {Key: " << key << ", Value: " << value << "}" << endl;
+    } else {
+        cout << "Not found Key: " << key << endl;
+    }
+}
+
+void test_put()
+{
+    RBTree memtable(6); // Example with memtable_size 6
+    cout << "--- test case 1:  Test put() without exceeding tree capacity ---" << endl;
+    // random insert
+    memtable.put(1, 10);
+    memtable.put(5, 50);
+    memtable.put(2, 20);
+    memtable.put(4, 40);
+    memtable.put(3, 30);
+    memtable.put(8, 80);
+    cout << "\n--- test case 2: Test put() with exceeding tree capacity ---" << endl;
+    memtable.put(-1, 80);
+}
+
+void test_scan(){
+    RBTree memtable(6); // Example with memtable_size 6
+    cout << "--- test case 1: Test scan() from memtable ---" << endl;
+    // random insert
+    memtable.put(1, 10);
+    memtable.put(5, 50);
+    memtable.put(2, 20);
+    memtable.put(4, 40);
+    memtable.put(3, 30);
+    memtable.put(8, 80);
+    double key1 = 2;
+    double key2 = 4;
+    vector<pair<int64_t, int64_t>> values = memtable.scan(key1, key2);
+    for (const auto& pair : values) {
+        cout << "Found {Key: " << pair.first << ", Value: " << pair.second << "}" << endl;
+    }
+
+    cout << "\n--- test case 2: Test scan() from SST ---" << endl;
+    memtable.put(-1, 80);
+    values = memtable.scan(key1, key2);
+    for (const auto& pair : values) {
+        cout << "Found {Key: " << pair.first << ", Value: " << pair.second << "}" << endl;
+    }
+}
+
 void unit_test1()
 {
     RBTree memtable(6); // Example with memtable_size 6
@@ -73,6 +137,11 @@ void unit_test1()
 }
 
 int main(int argc, char **argv) {
-    unit_test1();
+    cout << "\n===== Test Put(key, value) =====\n" << endl;
+    test_put();
+    cout << "\n===== Test Get(key) =====\n" << endl;
+    test_get();
+    cout << "\n===== Test Scan(Key1, Key2) =====\n" << endl;
+    test_scan();
     return 0;
 }
