f5920e62fc8250fb212b8cdb3875efd488fa755c
Weizhou Wang <weizhou.wang@mail.utoronto.ca>
2023-10-08 16:03:43 -0400

    Added db.cpp back

---
+++ src/db.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "rbtree.h"
+#include <fstream>
+#include <cassert>
+#include <fcntl.h>
+#include <unistd.h>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Running benchmarking..." << endl;
+}
