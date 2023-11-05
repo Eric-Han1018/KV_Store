67f7d8de174bf8787799c9c6dc5ccc32df116c6d
eric-han <dechen.han@mail.utoronto.ca>
2023-11-05 17:02:31 -0500

    update bufferpool

--- include/bufferpool.h
+++ include/bufferpool.h
@@ -1,26 +1,47 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <list>
 #include <filesystem>
 #include <set>
 #include <algorithm>
 #include "constants.h"
+#include "MurmurHash3.h"
+#include "rbtree.h"
+
 using namespace std;
 namespace fs = std::filesystem;
+
+// Define the page size in bytes (4KB)
+const size_t PAGE_SIZE = 4 * 1024;
+
+class Frame {
+    public:
+        string p_id;
+        // std::array<int64_t, PAGE_SIZE> page;
+        string page;
+        
+        Frame(string p_id){
+            p_id = p_id;
+            // page.fill(0);
+        }
+};
+
 
 class Bufferpool {
     public:
         size_t current_size;
         size_t maximal_size;
+        vector<list<Frame>> hash_directory;
 
-        Bufferpool(size_t initial_size, size_t maximal_size) {
-            current_size = initial_size;
-            maximal_size = maximal_size;
-        }
+        Bufferpool(size_t initial_size, size_t maximal_size):
+            current_size(initial_size), maximal_size(maximal_size), hash_directory(initial_size) {}
 
         ~Bufferpool() {}
 
     private:
         void change_maximal_size(size_t new_maximal_size);
-        
+        Result get(int64_t*& result, const string& p_id);
+        void insert(const string& p_id, string data);
+        size_t murmur_hash(const string& key);
 };
--- src/bufferpool.cpp
+++ src/bufferpool.cpp
@@ -10,4 +10,39 @@
 #include <limits>
 #include <string>
 #include <sstream>
+#include "bufferpool.h"
+
 using namespace std;
+
+size_t Bufferpool::murmur_hash(const string& key){
+    uint32_t seed = 443;
+    const int length = key.length();
+    uint32_t hash;
+    // wolf:~$ uname -m => x86_64
+    MurmurHash3_x64_128(key.c_str(), length, seed, &hash);
+    return static_cast<size_t>(hash);
+}
+
+void Bufferpool::change_maximal_size(size_t new_size){
+    if (new_size < current_size) {
+        // TODO: evict
+        printf(" ");
+    }
+    maximal_size = new_size;
+}
+
+Result Bufferpool::get(int64_t*& result, const string& p_id){
+    size_t bucket_index = murmur_hash(p_id) % hash_directory.size();
+    for (auto frame = hash_directory[bucket_index].begin(); frame != hash_directory[bucket_index].end(); ++frame) {
+        if (frame->p_id == p_id) {
+            result = frame->page;
+            return result;
+        }
+    }
+    return NotInBufferPool;
+}
+
+void insert(const string& p_id, string data) {
+    size_t bucket_index = murmur_hash(p_id) % hash_directory.size();
+
+}
